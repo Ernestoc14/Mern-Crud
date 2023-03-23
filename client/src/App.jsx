0c3fd461f45a26f7cac01a73957f426f933491f6
Ernestoc14 <descreed34@hotmail.com>
2023-03-23 00:39:06 -0500

    Add Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-import { HomePage, PostForm , NotFoundPage} from "./pages/index";
+import { HomePage, PostForm, NotFoundPage } from "./pages/index";
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  return(
-    <div>Hello World</div>
+  return (
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container m-auto">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/new' element={<PostForm />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
