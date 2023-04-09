46ae57731d0f315f7e67136e26dba868f1069bfb
Ernestoc14 <descreed34@hotmail.com>
2023-04-09 15:07:52 -0500

    ESLint rules and Context API

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
+import { PostProvider } from './context/postContext'
 import { HomePage, PostForm, NotFoundPage } from './pages'
 import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
 	return (
 		<div className='bg-neutral-900 min-h-screen flex items-center'>
-			<div className='m-auto'>
-				<Routes>
-					<Route path='/' element={<HomePage />} />
-					<Route path='/new' element={<PostForm />} />
-					<Route path='*' element={<NotFoundPage />} />
-				</Routes>
+			<div className='m-auto '>
+				<PostProvider>
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/new' element={<PostForm />} />
+						<Route path='*' element={<NotFoundPage />} />
+					</Routes>
+				</PostProvider>
 			</div>
 		</div>
 	)
---
+++ client/src/context/postContext.js
@@ -0,0 +1,18 @@
+import { useState, useContext, createContext } from 'react'
+
+const postContext = createContext()
+
+export const usePosts = () => {
+    const context = useContext(postContext)
+    return context
+}
+
+export const PostProvider = ({ children }) => {
+	const [posts, setPosts] = useState([])
+
+	return <postContext.Provider value={{
+        posts,
+    }}>
+        {children}
+    </postContext.Provider>
+}
